4695ee45704334ada467e63752daeedcee5332ca
lenrd <leonardo_aguiar@live.co.kr>
2020-05-03 23:30:52 -0300

    ensure LoginRouter return 400 if password is not provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return { statusCode: 400 }
     }
   }
@@ -13,6 +13,19 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'Qualquer coisa'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+describe('Login Router', () => {
+  test('Should return 400 if no   is provided', () => {
+    // Sut: system under test. indica o sistema a ser testado!
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'Qualquer@coisa.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
