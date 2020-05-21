953266b8fbda5ff529b8698222187ee978172d42
lenrd <leonardo_aguiar@live.co.kr>
2020-05-20 23:53:38 -0300

    moved errors to its own folder

---
+++ src/presentation/errors/index.js
@@ -0,0 +1,11 @@
+const MissingParamError = require('./missingParamError')
+const InvalidParamError = require('./InvalidParamError')
+const ServerError = require('./ServerError')
+const UnauthorizedError = require('./UnauthorizedError')
+
+module.exports = {
+  MissingParamError,
+  InvalidParamError,
+  ServerError,
+  UnauthorizedError
+}
